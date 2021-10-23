38582848499e153fa24420b0f0228430a4013f11
Valerie <valerie.tram.anh.n@gmail.com>
2021-10-23 16:24:27 -0400

    added basic inquirer questions for employee class

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 const Employee = require("./lib/Employee")
+const inquirer = require('inquirer');
 
-let yogi = new Employee("Dhalsim", 20, 65); 
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the employee?', 
+      },
+      {
+        type: 'number',
+        name: 'id',
+        message: 'Please enter the ID of the employee.'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+      }
+    ]);
+};
+  
+// TODO: Create a function to write README file
+// TODO: Create a function to initialize app
+// Function call to initialize app
+promptUser()
+// .then(readMeData => {
+//     const readMe = generatePage(readMeData);
+//     fs.writeFile('./sampleREADME.md', readMe, err => {
+//       if (err) throw new Error(err);
+
+//       console.log('Readme complete! Check out your readme.md to see the output!');
+//     });
+// });
